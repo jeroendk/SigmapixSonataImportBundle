57d8d9de1fe1823f4d69405649079249cddf7ff4
Jeroen de Kok <jeroen@aveq.nl>
2018-02-16 16:18:36 +0100

    Removed exportere overide.

--- DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -9,6 +9,6 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $definition = $container->getDefinition('sonata.admin.exporter');
-        $definition->setClass('Sigmapix\Sonata\ImportBundle\Export\Exporter');    }
+        
+	}
 }
